6632bf436147e4809b19171c3d7c9c461025ddce
user <user@lubuntu>
2015-11-02 11:30:39 +0100

    youpi j'adore cette matière

---
+++ monCul/src/main/java/monCul/monCul/App.java
@@ -0,0 +1,13 @@
+package monCul.monCul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
